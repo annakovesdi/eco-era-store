250e36712e5cd036a9d68cbab024117382f1fb19
annakovesdi <annakovesdi@gmail.com>
2021-07-05 15:50:54 +0000

    added hero video to home app

--- products/admin.py
+++ products/admin.py
@@ -3,22 +3,23 @@ from .models import Product, Category
 
 # Register your models here.
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
-        'name', 
+        'name',
         'category',
         'price',
         'rating',
     )
 
-    ordering = ('name',)
+    ordering = ('category',)
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
-    )  
-
+    )
 
 
 admin.site.register(Product, ProductAdmin)
