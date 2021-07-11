557f7a644c6b5b2f02f47e1bfae61f2d8c89a942
annakovesdi <annakovesdi@gmail.com>
2021-07-11 14:30:52 +0000

    added bag editing fuctionality

--- shoppingbag/contexts.py
+++ shoppingbag/contexts.py
@@ -24,7 +24,7 @@ def bag_contents(request):
         delivery = 0
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD
     elif total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total + Decimal(settings.STANDARD_DELIVERY)
+        delivery = Decimal(settings.STANDARD_DELIVERY)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
