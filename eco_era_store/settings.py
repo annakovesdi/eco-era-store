0fe9e488502db65a3be97b9aa140c904e81d6f48
annakovesdi <annakovesdi@gmail.com>
2021-08-15 10:30:54 +0000

    corrected aws bucket name typo

--- eco_era_store/settings.py
+++ eco_era_store/settings.py
@@ -181,7 +181,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'eco_era_store'
+    AWS_STORAGE_BUCKET_NAME = 'eco-era-store'
     AWS_S3_REGION_NAME = 'eu-central-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
